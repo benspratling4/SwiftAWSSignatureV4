173ca63bea5750880701892fe3ac6e4b24f8e0cd
Ben Spratling IV <bspratling@mac.com>
2017-05-14 01:46:23 -0400

    Correct unsigned payload behavior

--- Sources/URLRequest+AWSAccountSigning.swift
+++ Sources/URLRequest+AWSAccountSigning.swift
@@ -37,7 +37,7 @@ extension URLRequest {
 		//add some headers
 		addPreAuthHeaders(for:AWSAccount, date:now, signPayload:signPayload)
 		//auth header
-		let header = newAuthorizationHeader(AWSAccount: AWSAccount, now: now, nowComponents: nowComponents)
+		let header = newAuthorizationHeader(AWSAccount: AWSAccount, now: now, nowComponents: nowComponents, signPayload:signPayload)
 		setValue(header, forHTTPHeaderField: "Authorization")
 	}
 	
@@ -93,19 +93,19 @@ extension URLRequest {
 	}
 	
 	
-	func canonicalRequest(now:Date)->(request:String, signedHeaders:String)? {
+	func canonicalRequest(now:Date, signPayload:Bool)->(request:String, signedHeaders:String)? {
 		let verb:String = httpMethod ?? "GET"
 		guard var uriString:String = url?.path else { return nil } 	//TODO: "URI Encode"
 		var queryString:String? = url?.query
 		if queryString != nil {
 			uriString.append("?")
 		}
-		guard let encodedURI:String = uriString.uriEncoded(encodeSlash: false) else { return nil }
+		guard let encodedURI:String = uriString.aws_uriEncoded(encodeSlash: false) else { return nil }
 		if let queryLongString = queryString {
 			let queryItems:[String] = queryLongString.components(separatedBy: "&")
 			let reconstituted:[String] = queryItems.map{
 				$0.components(separatedBy: "=")
-					.flatMap{$0.uriEncoded(encodeSlash: true)}
+					.flatMap{$0.aws_uriEncoded(encodeSlash: true)}
 				.joined(separator: "=")}
 			queryString = reconstituted.joined(separator: "&")
 		}
@@ -117,8 +117,8 @@ extension URLRequest {
 		headers.append("\n")
 		let signedHeaders:String = headerValues.map({$0.0}).joined(separator: ";")
 		
-		let hashedBody:String = sha256HashedBody.map { CryptoUtils.hexString(from: $0) }
-		?? "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+		let hashedBody:String = signPayload ? sha256HashedBody.map { CryptoUtils.hexString(from: $0) }
+		?? "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" : "UNSIGNED-PAYLOAD"
 		
 		return ([verb, encodedURI, queryString ?? "", headers, signedHeaders, hashedBody].joined(separator: "\n"), signedHeaders)
 	}
@@ -133,9 +133,10 @@ extension URLRequest {
 	}
 	
 	
-	func stringToSign(AWSAccount:AWSAccount, now:Date, nowComponents:DateComponents)->(string:String, signedHeaders:String)? {
+	func stringToSign(AWSAccount:AWSAccount, now:Date, nowComponents:DateComponents, signPayload:Bool)->(string:String, signedHeaders:String)? {
 		let timeString:String = HTTPDate(now: nowComponents)
-		guard let (request, signedHeaders) = canonicalRequest(now:now) else { return nil }
+		guard let (request, signedHeaders) = canonicalRequest(now:now, signPayload:signPayload) else { return nil }
+		print("canonical request = \(request)")
 		let hashOfCanonicalRequest:[UInt8] = Digest(using: .sha256).update(string: request)?.final() ?? []
 		let hexHash:String = CryptoUtils.hexString(from: hashOfCanonicalRequest)
 		
@@ -143,10 +144,11 @@ extension URLRequest {
 	}
 	
 	
-	func newAuthorizationHeader(AWSAccount:AWSAccount, now:Date, nowComponents:DateComponents)->String? {
+	func newAuthorizationHeader(AWSAccount:AWSAccount, now:Date, nowComponents:DateComponents, signPayload:Bool = false)->String? {
 		guard let signingKey:[UInt8] = AWSAccount.keyForSigning(now:nowComponents)
-			,let (string, signedHeaders) = stringToSign(AWSAccount:AWSAccount, now:now, nowComponents:nowComponents)
+			,let (string, signedHeaders) = stringToSign(AWSAccount:AWSAccount, now:now, nowComponents:nowComponents, signPayload:signPayload)
 			else { return nil }
+		print("string to sign = \(string)")
 		let signature:[UInt8] = HMAC(using:HMAC.Algorithm.sha256, key: Data(signingKey)).update(byteArray: CryptoUtils.byteArray(from:string))!.final()
 		let signatureHex:String = CryptoUtils.hexString(from: signature)
 		
